3a091114c5cef27ba6b798b416d2ace700e17e69
Jérôme Respaut <jerome.respaut@fr.clara.net>
2022-10-26 09:58:34 +0200

    AZ-886: For_each

--- r-vpn.tf
+++ r-vpn.tf
@@ -89,7 +89,7 @@ resource "azurerm_virtual_network_gateway_connection" "virtual_network_gateway_c
 }
 
 resource "random_password" "vpn_ipsec_shared_key" {
-  for_each = var.vpn_connections
+  for_each = { for c in var.vpn_connections : c.name => c }
   length   = 32
   special  = false
 }
